dfe9e2d9b6895f1a0d8b925fae89c4e9ad9f2f65
Kishan Suvarna <kishansuvarna09@gmail.com>
2021-06-19 06:30:12 +0530

    gitignore update

--- scripts/gitbranch.sh
+++ scripts/gitbranch.sh
@@ -23,14 +23,19 @@ function pushBranch() {
     read message
     git add .
     git commit -m"${message}"
-    if [ -n "$(git status - porcelain)" ];
-    then
-        echo "IT IS CLEAN"
-    else
-        git status
-        echo "Pushing data to remote server!!!"
-        git push origin $1
-    fi
+
+    git status
+    echo "Pushing data to remote server!!!"
+    git push origin $1
+
+    # if [ -n "$(git status - porcelain)" ];
+    # then
+    #     echo "IT IS CLEAN"
+    # else
+    #     git status
+    #     echo "Pushing data to remote server!!!"
+    #     git push origin $1
+    # fi
 }
 
 pushBranch main
