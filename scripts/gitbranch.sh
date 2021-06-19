248c7538238a626dcddd9b67f4587e4223fa89c6
Kishan Suvarna <kishansuvarna09@gmail.com>
2021-06-19 06:29:40 +0530

    gitignore update

---
+++ scripts/gitbranch.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+function addSshKeysToAgent() {
+  echo "starting ssh agent:"
+  echo ""
+
+  eval `ssh-agent -s`
+  echo ""
+
+  echo "adding identities: "
+  ssh-add /e/personal_keys/kishan_personal
+  echo ""
+
+  echo "list ssh key agents"
+  ssh-add -l
+
+  echo "ssh keys added to ssh agent."
+}
+
+function pushBranch() {
+    addSshKeysToAgent
+    echo "Enter your message"
+    read message
+    git add .
+    git commit -m"${message}"
+    if [ -n "$(git status - porcelain)" ];
+    then
+        echo "IT IS CLEAN"
+    else
+        git status
+        echo "Pushing data to remote server!!!"
+        git push origin $1
+    fi
+}
+
+pushBranch main
